ecd1cf0411b32e623975d572200d61abf4269946
ajmac216 <ajmac216@yahoo.com>
2023-11-15 19:11:18 -0600

    safd

--- main_js.js
+++ main_js.js
@@ -6,9 +6,9 @@ $("#load").delay(2000).fadeOut(200);
 
 $("#phone_account").fadeOut(0);
 
-$("#phone_home").delay(5000).fadeOut(0);
+//$("#phone_home").delay(5000).fadeOut(0);
 
-$("#phone_account").delay(5000).fadeIn(0);
+//$("#phone_account").delay(5000).fadeIn(0);
 
 
 });
