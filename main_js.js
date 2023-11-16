a58a278a68acb150487664c7c2f7506ef5257c04
ajmac216 <ajmac216@yahoo.com>
2023-11-15 19:05:22 -0600

    account and stuff maybe

--- main_js.js
+++ main_js.js
@@ -2,7 +2,13 @@
 
 document.addEventListener("DOMContentLoaded", function(event){
 
-$("#load").delay(2000).fadeOut(200);;
+$("#load").delay(2000).fadeOut(200);
+
+$("#phone_account").fadeOut(0);
+
+$("#phone_home").delay(5000).fadeOut(0);
+
+$("#phone_account").delay(5000).fadeIn(0);
 
 
 });
